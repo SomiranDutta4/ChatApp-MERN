170734ff293b7d1aaea354d8002e7d2ddb87e40d
SomiranDutta4 <somirandutta69@gmail.com>
2025-03-18 10:42:32 +0530

    minute

--- server/index.js
+++ server/index.js
@@ -83,7 +83,8 @@ const server = app.listen(PORT, function (err) {
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin:'*',
+    // origin: "http://localhost:3000",
     // credentials: true,
   }
 })
