c50e6bd1ff607db7012bc710e6bb6ed7adc76e59
SomiranDutta4 <somirandutta69@gmail.com>
2025-03-19 11:55:58 +0530

    done

--- server/config/JWTs.js
+++ server/config/JWTs.js
@@ -3,7 +3,7 @@ const jwt=require('jsonwebtoken')
 module.exports=function(id){
     const token=jwt.sign({
         _id:id
-        },process.env.ChatApp_Secret,{expiresIn:'1h'}
+        },process.env.ChatApp_Secret,{expiresIn:'7d'}
       )
       return token
 }
